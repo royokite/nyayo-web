40622510b184e353ece7f4c3237757e4635cdeda
Roy Okite <royokite@gmail.com>
2023-06-19 14:54:05 +0300

    Add the sidenav component
    Add the sidenav to the project for navigation

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
+  exports: [
+    CommonModule,
+    MatListModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule
   ]
 })
 export class MaterialModule { }
