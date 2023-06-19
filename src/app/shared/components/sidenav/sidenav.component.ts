01495bc33706f13e88fb94ee93340ff35eaf7906
Roy Okite <royokite@gmail.com>
2023-06-19 22:49:00 +0300

    Update sidenav menu items
    Set up the standard menu items for navigation

--- src/app/shared/components/sidenav/sidenav.component.ts
+++ src/app/shared/components/sidenav/sidenav.component.ts
@@ -8,10 +8,10 @@ import { MediaMatcher } from '@angular/cdk/layout';
 })
 export class SidenavComponent implements OnDestroy {
   mobileQuery: MediaQueryList;
+  opened: boolean = true;
+  sideNavList: any[] = [];
 
-  fillerNav = Array.from({ length: 10 }, (_, i) => `Nav Item ${i + 1}`);
-
-  fillerContent = Array.from(
+  sidenavContent = Array.from(
     { length: 10 },
     () =>
       `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
@@ -27,6 +27,33 @@ export class SidenavComponent implements OnDestroy {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addEventListener('change', this._mobileQueryListener);
+
+    this.sideNavList = [
+      { name: 'Dashboard', route: '/dashboard', icon: 'dashboard' },
+      {
+        name: 'Collections', route: '/collections', icon: 'shopping_basket', dropdown: true, children: [
+          { name: 'Supervisor', route: '', icon: 'info' },
+          { name: 'Driver', route: '', icon: 'info' },
+          { name: 'Factory', route: '', icon: 'info' },
+          { name: 'Tally', route: '', icon: 'info' }
+        ]
+      },
+      { name: 'Payments', route: '/payments', icon: 'payment' },
+      {
+        name: 'Reports', route: '', icon: 'bar_chart', dropdown: true, children: [
+          { name: 'Area', route: '', icon: '' },
+          { name: 'Personnel', route: '', icon: '' },
+          { name: 'Payments', route: '', icon: '' }
+        ]
+      },
+      {
+        name: 'Users', route: '/users', icon: 'supervisor_account', dropdown: true, children: [
+          { name: 'Casuals', route: '', icon: '' },
+          { name: 'Staff', route: '', icon: '' }
+        ]
+      },
+      { name: 'System Settings', route: '/settings', icon: 'settings' },
+    ]
   }
 
   ngOnDestroy(): void {
